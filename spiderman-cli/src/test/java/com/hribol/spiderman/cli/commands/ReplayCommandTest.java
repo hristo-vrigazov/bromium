8e059b617dd104d1a5b668979d65f7095bcb5e4b
Hristo Vrigazov <hvrigazov@gmail.com>
2017-04-28 19:42:00 +0300

    Remove test for replay command, since it is unreliable

--- spiderman-cli/src/test/java/com/hribol/spiderman/cli/commands/ReplayCommandTest.java
+++ spiderman-cli/src/test/java/com/hribol/spiderman/cli/commands/ReplayCommandTest.java
@@ -7,12 +7,4 @@ import org.junit.Test;
  */
 public class ReplayCommandTest {
 
-    @Test
-    public void asdasds() {
-        String pathToChromeDriver = "/home/hvrigazov/github/spiderman/chromedriver";
-        String pathToApplicationConfiguration = "/home/hvrigazov/github/spiderman/spiderman-core/src/test/resources/tenniskafe.json";
-        String testCaseFile = "/home/hvrigazov/github/spiderman/spiderman-core/src/test/resources/testCase.json";
-        Command command = new ReplayCommand(pathToChromeDriver, pathToApplicationConfiguration, testCaseFile);
-        command.run();
-    }
 }
