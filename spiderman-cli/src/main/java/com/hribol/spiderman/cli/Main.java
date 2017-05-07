88f58b55fe943e694f8e004ff361036f6a0a44ad
Hristo Vrigazov <hvrigazov@gmail.com>
2017-05-07 20:36:29 +0300

    Change underlying implementation in the example

--- spiderman-cli/src/main/java/com/hribol/spiderman/cli/Main.java
+++ spiderman-cli/src/main/java/com/hribol/spiderman/cli/Main.java
@@ -13,11 +13,11 @@ public class Main {
         String pathToApplicationConfiguration = "/home/hvrigazov/github/spiderman/spiderman-core/src/test/resources/tenniskafe.json";
         String baseUrl = "http://tenniskafe.com";
         String testCaseFile = "/home/hvrigazov/github/spiderman/spiderman-core/src/test/resources/testCase.json";
-//        Command command = new ReplayCommand(pathToChromeDriver, pathToApplicationConfiguration, testCaseFile,
-//                "measurements.csv", 10, 500,
-//                "http://tenniskafe.com");
-        Command command = new RecordCommand(pathToChromeDriver, pathToJSInjectionFile, baseUrl,
-                testCaseFile, new PromptUtils());
+        Command command = new ReplayCommand(pathToChromeDriver, pathToApplicationConfiguration, testCaseFile,
+                "measurements.csv", 10, 500,
+                "http://tenniskafe.com");
+//        Command command = new RecordCommand(pathToChromeDriver, pathToJSInjectionFile, baseUrl,
+//                testCaseFile, new PromptUtils());
         command.run();
         System.exit(0);
     }
