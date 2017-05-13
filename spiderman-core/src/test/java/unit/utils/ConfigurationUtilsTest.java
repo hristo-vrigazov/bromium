ae45b8ba6bf59ae01677824e00b3d30fe0232aaa
Hristo Vrigazov <hvrigazov@gmail.com>
2017-05-13 14:17:56 +0300

    Fix test

--- spiderman-core/src/test/java/unit/utils/ConfigurationUtilsTest.java
+++ spiderman-core/src/test/java/unit/utils/ConfigurationUtilsTest.java
@@ -77,7 +77,7 @@ public class ConfigurationUtilsTest {
         Map<String, String> firstStep = testCaseSteps.get(0);
         String firstEvent = firstStep.get("event");
 
-        assertEquals("clickMegaMenu", firstEvent);
+        assertEquals("Click on the logo 1", firstEvent);
     }
 
     @Test
