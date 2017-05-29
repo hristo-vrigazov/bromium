174799dc35273296b47213f855b26f225364c98f
Hristo Vrigazov <hvrigazov@gmail.com>
2017-05-29 23:03:55 +0300

    Make RecordResponseFilter tests independent of Utils implementation

--- spiderman-record/src/test/java/com/hribol/spiderman/record/RecordResponseFilterTest.java
+++ spiderman-record/src/test/java/com/hribol/spiderman/record/RecordResponseFilterTest.java
@@ -1,12 +1,16 @@
 package com.hribol.spiderman.record;
 
-import com.hribol.spiderman.record.RecordResponseFilter;
+import com.hribol.spiderman.replay.config.utils.Utils;
 import io.netty.handler.codec.http.HttpHeaders;
 import io.netty.handler.codec.http.HttpRequest;
 import io.netty.handler.codec.http.HttpResponse;
 import net.lightbody.bmp.util.HttpMessageContents;
 import net.lightbody.bmp.util.HttpMessageInfo;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -16,6 +20,11 @@ import static org.mockito.Mockito.*;
 /**
  * Created by hvrigazov on 27.04.17.
  */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({
+        Utils.class,
+        RecordResponseFilter.class
+})
 public class RecordResponseFilterTest {
 
     @Test
@@ -24,14 +33,16 @@ public class RecordResponseFilterTest {
         HttpResponse httpResponse = mock(HttpResponse.class);
         HttpMessageContents httpMessageContents = mock(HttpMessageContents.class);
         HttpHeaders httpHeaders = mock(HttpHeaders.class);
-        when(httpHeaders.get("Accept")).thenReturn("html");
         HttpRequest httpRequest = mock(HttpRequest.class);
         when(httpRequest.headers()).thenReturn(httpHeaders);
-        when(httpRequest.getUri()).thenReturn("http://tenniskafe.com/a?p=a");
         HttpMessageInfo httpMessageInfo = mock(HttpMessageInfo.class);
         when(httpMessageInfo.getOriginalRequest()).thenReturn(httpRequest);
         URI baseURI = new URI("http://tenniskafe.com");
         RecordResponseFilter recordResponseFilter = new RecordResponseFilter(baseURI, jsInjectionCode);
+
+        PowerMockito.mockStatic(Utils.class);
+        when(Utils.isGETFromCurrentHostAndAcceptsHTML(baseURI, httpRequest)).thenReturn(true);
+
         recordResponseFilter.filterResponse(httpResponse, httpMessageContents, httpMessageInfo);
 
         verify(httpMessageContents).setTextContents(jsInjectionCode + httpMessageContents.getTextContents());
@@ -42,14 +53,15 @@ public class RecordResponseFilterTest {
         String jsInjectionCode = "<script>var something = 5;</script>";
         HttpResponse httpResponse = mock(HttpResponse.class);
         HttpMessageContents httpMessageContents = mock(HttpMessageContents.class);
-        HttpHeaders httpHeaders = mock(HttpHeaders.class);
-        when(httpHeaders.get("Accept")).thenReturn("html");
         HttpRequest httpRequest = mock(HttpRequest.class);
-        when(httpRequest.headers()).thenReturn(httpHeaders);
-        when(httpRequest.getUri()).thenReturn("http://google.com/a?p=a");
         HttpMessageInfo httpMessageInfo = mock(HttpMessageInfo.class);
         when(httpMessageInfo.getOriginalRequest()).thenReturn(httpRequest);
+
         URI baseURI = new URI("http://tenniskafe.com");
+
+        PowerMockito.mockStatic(Utils.class);
+        when(Utils.isGETFromCurrentHostAndAcceptsHTML(baseURI, httpRequest)).thenReturn(false);
+
         RecordResponseFilter recordResponseFilter = new RecordResponseFilter(baseURI, jsInjectionCode);
         recordResponseFilter.filterResponse(httpResponse, httpMessageContents, httpMessageInfo);
 
