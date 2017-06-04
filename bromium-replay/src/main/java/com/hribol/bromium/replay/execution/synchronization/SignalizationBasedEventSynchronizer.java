716d452d7bb60c2b2138e29a284ba70bbe349ba6
Hristo Vrigazov <hvrigazov@gmail.com>
2017-06-05 00:15:38 +0300

    GH-20: Added tests for synchronizer

--- bromium-replay/src/main/java/com/hribol/bromium/replay/execution/synchronization/SignalizationBasedEventSynchronizer.java
+++ bromium-replay/src/main/java/com/hribol/bromium/replay/execution/synchronization/SignalizationBasedEventSynchronizer.java
@@ -13,8 +13,16 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class SignalizationBasedEventSynchronizer implements EventSynchronizer {
 
-    private Lock lock = new ReentrantLock();
+    private Lock lock;
     private Map<SynchronizationEvent, Condition> eventConditionMap = new HashMap<>();
+
+    public SignalizationBasedEventSynchronizer() {
+        this(new ReentrantLock());
+    }
+
+    public SignalizationBasedEventSynchronizer(Lock lock) {
+        this.lock = lock;
+    }
 
     @Override
     public void awaitUntil(SynchronizationEvent synchronizationEvent, int timeoutInSeconds) throws InterruptedException, TimeoutException {
@@ -30,7 +38,6 @@ public class SignalizationBasedEventSynchronizer implements EventSynchronizer {
         boolean timedOut = !condition.await(timeoutInSeconds, TimeUnit.SECONDS);
 
         lock.unlock();
-
 
         if (timedOut) {
             throw new TimeoutException("The synchronization event " + synchronizationEvent.getName() + " was not satisfied in the specified time");
---
+++ bromium-replay/src/test/java/com/hribol/bromium/replay/execution/synchronization/SignalizationBasedEventSynchronizerTest.java
@@ -0,0 +1,79 @@
+package com.hribol.bromium.replay.execution.synchronization;
+
+import com.hribol.bromium.replay.config.utils.Utils;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * Created by hvrigazov on 04.06.17.
+ */
+public class SignalizationBasedEventSynchronizerTest {
+
+    private int timeout = 2;
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void awaitForEventDoesNotLockTheThreadIfEventIsAlreadySatisfied() throws TimeoutException, InterruptedException {
+        SynchronizationEvent synchronizationEvent = mock(SynchronizationEvent.class);
+        when(synchronizationEvent.isAlreadySatisfied()).thenReturn(true);
+        Lock lock = mock(Lock.class);
+
+        SignalizationBasedEventSynchronizer signalizationBasedEventSynchronizer = new SignalizationBasedEventSynchronizer(lock);
+        signalizationBasedEventSynchronizer.awaitUntil(synchronizationEvent, timeout);
+
+        verify(lock, never()).lock();
+    }
+
+    @Test
+    public void exceptionIsThrownAndEventIsCleanedUpIfAwaitTimesOut() throws TimeoutException, InterruptedException {
+        SynchronizationEvent synchronizationEvent = mock(SynchronizationEvent.class);
+        when(synchronizationEvent.isAlreadySatisfied()).thenReturn(false);
+
+        Condition condition = mock(Condition.class);
+        // condition#await returns false if time was exceeded
+        when(condition.await(timeout, TimeUnit.SECONDS)).thenReturn(false);
+        Lock lock = mock(Lock.class);
+        when(lock.newCondition()).thenReturn(condition);
+
+        SignalizationBasedEventSynchronizer signalizationBasedEventSynchronizer = new SignalizationBasedEventSynchronizer(lock);
+
+        expectedException.expect(TimeoutException.class);
+        signalizationBasedEventSynchronizer.awaitUntil(synchronizationEvent, timeout);
+    }
+
+    @Test
+    public void ifEventIsSignalizedThenLockIsUnlocked() throws TimeoutException, InterruptedException {
+        SynchronizationEvent synchronizationEvent = mock(SynchronizationEvent.class);
+        when(synchronizationEvent.isAlreadySatisfied()).thenReturn(false);
+
+        SignalizationBasedEventSynchronizer signalizationBasedEventSynchronizer = new SignalizationBasedEventSynchronizer();
+
+        Thread signalizingThread = new Thread(() -> {
+            try {
+                // simulate some work
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            signalizationBasedEventSynchronizer.signalizeEvent(synchronizationEvent);
+        });
+        signalizingThread.start();
+        signalizationBasedEventSynchronizer.awaitUntil(synchronizationEvent, timeout);
+        signalizingThread.join();
+    }
+
+    @Test
+    public void 
+}
