eaedba78593905ab84cbf1c0789525ae12cf36d0
Hristo Vrigazov <hvrigazov@gmail.com>
2017-06-15 08:10:20 +0300

    GH-26: Add test for WebDriverActionExecutionException

--- bromium-replay/src/test/java/com/hribol/bromium/replay/execution/WebDriverActionExecutionExceptionTest.java
+++ bromium-replay/src/test/java/com/hribol/bromium/replay/execution/WebDriverActionExecutionExceptionTest.java
@@ -1,6 +1,11 @@
 package com.hribol.bromium.replay.execution;
 
+import com.hribol.bromium.replay.report.AutomationResult;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by hvrigazov on 14.06.17.
@@ -8,5 +13,19 @@ import org.junit.Test;
 public class WebDriverActionExecutionExceptionTest {
 
     @Test
-    public void
+    public void returnsTheCorrectAutomationResult() {
+        String message = "Some message";
+        Throwable e = mock(Throwable.class);
+        AutomationResultBuilder automationResultBuilder = mock(AutomationResultBuilder.class);
+
+        AutomationResult expected = AutomationResult.TIMEOUT;
+        when(automationResultBuilder.buildAutomationResult(e)).thenReturn(expected);
+
+        WebDriverActionExecutionException webDriverActionExecutionException =
+                new WebDriverActionExecutionException(message, e, automationResultBuilder);
+
+        AutomationResult actual = webDriverActionExecutionException.getAutomationResult();
+
+        assertEquals(expected, actual);
+    }
 }
