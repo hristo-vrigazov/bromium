99600fcd06fca2c0e83810a5b2a80612dd157a26
Hristo Vrigazov <hvrigazov@gmail.com>
2017-06-15 08:38:40 +0300

    GH-26: Dummy test

---
+++ bromium-common/src/test/java/com/hribol/bromium/common/DummyTest.java
@@ -0,0 +1,17 @@
+package com.hribol.bromium.common;
+
+import com.hribol.bromium.common.record.builder.JsFunctionNames;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Created by hvrigazov on 15.06.17.
+ */
+public class DummyTest {
+
+    @Test
+    public void dummy() {
+        assertNotNull(new JsFunctionNames());
+    }
+}
