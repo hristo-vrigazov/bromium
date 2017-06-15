f8dd81aaa93b71714ee9e04609f118ec30bb1486
Hristo Vrigazov <hvrigazov@gmail.com>
2017-06-15 08:19:31 +0300

    GH-26: Adapted test in ActionWithJSPrecondition

--- bromium-common/src/test/java/com/hribol/bromium/common/replay/actions/ActionWithJSPreconditionBaseTest.java
+++ bromium-common/src/test/java/com/hribol/bromium/common/replay/actions/ActionWithJSPreconditionBaseTest.java
@@ -58,40 +58,44 @@ public class ActionWithJSPreconditionBaseTest {
         verify(eventSynchronizer).awaitUntil(any(SynchronizationEvent.class));
     }
 
-//    @Test
-//    public void lockDoesWaitIfWaitingEventIsNotSatisfiedWhenSubmitted() throws InterruptedException {
-//        String jsEvent = "jsEvent";
-//
-//        ActionWithJSPreconditionBase actionWithJSPreconditionBase = new ActionWithJSPreconditionBase() {
-//            @Override
-//            public void executeAfterJSPreconditionHasBeenSatisfied(WebDriver driver, ReplayFiltersFacade facade) {
-//
-//            }
-//
-//            @Override
-//            public String getName() {
-//                return null;
-//            }
-//
-//            @Override
-//            public boolean expectsHttpRequest() {
-//                return false;
-//            }
-//
-//            @Override
-//            public String getJSEventToWaitFor() {
-//                return jsEvent;
-//            }
-//        };
-//
-//        WebDriver driver = mock(WebDriver.class);
-//        ReplayRequestFilter replayRequestFilter = mock(ReplayRequestFilter.class);
-//        ReplayFiltersFacade facade = mock(ReplayFiltersFacade.class);
-//        when(facade.getRequestFilter()).thenReturn(replayRequestFilter);
-//        when(replayRequestFilter.isSatisfied(jsEvent)).thenReturn(true);
-//        actionWithJSPreconditionBase.execute(driver, facade);
-//
-//    }
+    @Test
+    public void lockDoesWaitIfWaitingEventIsNotSatisfiedWhenSubmitted() throws InterruptedException, TimeoutException {
+        String jsEvent = "jsEvent";
+
+        ActionWithJSPreconditionBase actionWithJSPreconditionBase = new ActionWithJSPreconditionBase() {
+            @Override
+            public void executeAfterJSPreconditionHasBeenSatisfied(WebDriver driver, ReplayFiltersFacade facade) {
+
+            }
+
+            @Override
+            public String getName() {
+                return null;
+            }
+
+            @Override
+            public boolean expectsHttpRequest() {
+                return false;
+            }
+
+            @Override
+            public String getJSEventToWaitFor() {
+                return jsEvent;
+            }
+        };
+
+        WebDriver driver = mock(WebDriver.class);
+        ReplayRequestFilter replayRequestFilter = mock(ReplayRequestFilter.class);
+        EventSynchronizer eventSynchronizer = mock(EventSynchronizer.class);
+        ReplayFiltersFacade facade = mock(ReplayFiltersFacade.class);
+        when(facade.getRequestFilter()).thenReturn(replayRequestFilter);
+        when(facade.getEventSynchronizer()).thenReturn(eventSynchronizer);
+        when(replayRequestFilter.isSatisfied(jsEvent)).thenReturn(true);
+        actionWithJSPreconditionBase.execute(driver, facade);
+
+        verify(eventSynchronizer).awaitUntil(any());
+
+    }
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
