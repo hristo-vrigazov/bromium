4ddb9a5bc9dc02354bdad29a10097efed6446509
Hristo Vrigazov <hvrigazov@gmail.com>
2017-07-03 20:55:52 +0300

    GH-26: Added tests for FunctionFactoryBase

---
+++ bromium-common/src/test/java/com/hribol/bromium/common/generation/record/RecorderFunctionRegistryTest.java
@@ -0,0 +1,50 @@
+package com.hribol.bromium.common.generation.record;
+
+import com.hribol.bromium.common.generation.helper.NameWebDriverActionConfiguration;
+import com.hribol.bromium.common.generation.helper.StepAndWebDriverActionConfiguration;
+import com.hribol.bromium.common.generation.record.functions.RecorderFunction;
+import com.hribol.bromium.common.generation.record.functions.RecorderFunctionInvocation;
+import com.hribol.bromium.common.generation.replay.BaseReplayFunctionFactory;
+import com.hribol.bromium.common.generation.replay.ReplayFunctionRegistry;
+import com.hribol.bromium.common.generation.replay.functions.ReplayFunction;
+import com.hribol.bromium.common.generation.replay.functions.ReplayFunctionInvocation;
+import com.hribol.bromium.core.config.WebDriverActionConfiguration;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by hvrigazov on 03.07.17.
+ */
+public class RecorderFunctionRegistryTest {
+
+    @Test
+    public void addsFunctionDeclarationCodeIfFunctionIsNotRegistered() {
+        // Arrange
+        String functionDefinition = "function something(a, b){}";
+        String functionInvocation = "something(2, 5)";
+        RecorderFunctionInvocation recorderFunctionInvocation = mock(RecorderFunctionInvocation.class);
+        when(recorderFunctionInvocation.getJavascriptCode()).thenReturn(functionInvocation);
+
+        RecorderFunction recorderFunction = mock(RecorderFunction.class);
+        when(recorderFunction.getJavascriptCode()).thenReturn(functionDefinition);
+
+        WebDriverActionConfiguration webDriverActionConfiguration = mock(WebDriverActionConfiguration.class);
+        NameWebDriverActionConfiguration generationInformation = mock(NameWebDriverActionConfiguration.class);
+        when(generationInformation.getGenerationFunctionInformation()).thenReturn(webDriverActionConfiguration);
+
+        BaseRecorderFunctionFactory baseReplayFunctionFactory = mock(BaseRecorderFunctionFactory.class);
+        when(baseReplayFunctionFactory.create(webDriverActionConfiguration)).thenReturn(recorderFunction);
+        when(recorderFunction.getInvocation(generationInformation)).thenReturn(recorderFunctionInvocation);
+
+        String expected = functionDefinition + functionInvocation;
+        // Act
+        RecorderFunctionRegistry replayTypeRegistry = new RecorderFunctionRegistry(baseReplayFunctionFactory);
+        String generatedString = replayTypeRegistry.generate(generationInformation);
+
+        // Assert
+        assertEquals(expected, generatedString);
+    }
+}
