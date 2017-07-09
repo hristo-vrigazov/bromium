382bc0b4f9de5db3b71f55ac10ba9b8c3772cf52
Hristo Vrigazov <hvrigazov@gmail.com>
2017-07-09 21:17:21 +0300

    GH-30: Added failsafe plugin

---
+++ bromium-cli/src/test/java/com/hribol/bromium/integration/tests/ExampleIT.java
@@ -0,0 +1,14 @@
+package com.hribol.bromium.integration.tests;
+
+import org.junit.Test;
+
+/**
+ * Created by hvrigazov on 09.07.17.
+ */
+public class ExampleIT {
+
+    @Test
+    public void something() {
+
+    }
+}
