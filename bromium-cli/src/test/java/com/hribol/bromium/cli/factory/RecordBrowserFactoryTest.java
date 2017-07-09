650cf0ebf792b11bcd202ff6129f8fbb25d913b9
Hristo Vrigazov <hvrigazov@gmail.com>
2017-07-09 08:47:25 +0300

    GH-29: Added unit test to RecordBrowserFactoryTest

--- bromium-cli/src/test/java/com/hribol/bromium/cli/factory/RecordBrowserFactoryTest.java
+++ bromium-cli/src/test/java/com/hribol/bromium/cli/factory/RecordBrowserFactoryTest.java
@@ -1,12 +1,29 @@
 package com.hribol.bromium.cli.factory;
 
+import com.hribol.bromium.browsers.chrome.record.ChromeRecordBrowser;
+import com.hribol.bromium.common.record.RecordBrowserBase;
+import com.hribol.bromium.core.utils.JavascriptInjector;
 import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.openqa.selenium.remote.BrowserType.CHROME;
+
 
 public class RecordBrowserFactoryTest {
 
-    private final String pathToRecordTemplateResource = "/record.js";
+    private final String pathToDriver = "/chromedriver";
 
     @Test
     public void canCreateChromeDriver() throws Exception {
+        JavascriptInjector javascriptInjector = mock(JavascriptInjector.class);
+
+        RecordBrowserFactory recordBrowserFactory = new RecordBrowserFactory();
+
+        RecordBrowserBase recordBrowserBase = recordBrowserFactory.create(CHROME, pathToDriver, javascriptInjector);
+
+        assertNotNull(recordBrowserBase);
+        assertTrue(recordBrowserBase instanceof ChromeRecordBrowser);
     }
 }
