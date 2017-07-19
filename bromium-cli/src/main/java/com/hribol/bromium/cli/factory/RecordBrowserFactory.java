92cb9141b5a04c15af111c5fd42b655baf43c548
Hristo Vrigazov <hvrigazov@gmail.com>
2017-07-19 08:28:16 +0300

    GH-30: Clean up useless class

--- bromium-cli/src/main/java/com/hribol/bromium/cli/factory/RecordBrowserFactory.java
+++ bromium-cli/src/main/java/com/hribol/bromium/cli/factory/RecordBrowserFactory.java
@@ -1,25 +1,14 @@
 package com.hribol.bromium.cli.factory;
 
 import com.google.inject.Inject;
-import com.google.inject.name.Named;
 import com.hribol.bromium.browsers.chrome.record.ChromeRecordBrowser;
-import com.hribol.bromium.common.generation.helper.suppliers.RecordingJavascriptGeneratorSupplier;
 import com.hribol.bromium.common.record.RecordBrowserBase;
-import com.hribol.bromium.core.config.ApplicationActionConfiguration;
-import com.hribol.bromium.core.config.ApplicationConfiguration;
-import com.hribol.bromium.core.generation.JavascriptGenerator;
-import com.hribol.bromium.core.suppliers.JavascriptInjectorSupplier;
-import com.hribol.bromium.core.utils.ConfigurationUtils;
 import com.hribol.bromium.core.utils.JavascriptInjector;
-import com.hribol.bromium.core.utils.parsing.ApplicationConfigurationParser;
-import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
-import java.io.StringReader;
 import java.util.HashMap;
 import java.util.Map;
 
-import static com.hribol.bromium.cli.Constants.RECORD_TEMPLATE_RESOURCE;
 import static org.openqa.selenium.remote.BrowserType.CHROME;
 
 /**
