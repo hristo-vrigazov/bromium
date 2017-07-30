2ad887ca55935971ca1761d8742cd96882c107aa
Hristo Vrigazov <hvrigazov@gmail.com>
2017-07-30 22:53:12 +0300

    GH-37: Clean up Utils

--- bromium-core/src/main/java/com/hribol/bromium/core/utils/Utils.java
+++ bromium-core/src/main/java/com/hribol/bromium/core/utils/Utils.java
@@ -1,23 +1,9 @@
 package com.hribol.bromium.core.utils;
 
-import io.netty.handler.codec.http.HttpMethod;
-import io.netty.handler.codec.http.HttpRequest;
-
-import java.net.URI;
-
-import static io.netty.handler.codec.http.HttpHeaders.Names.ACCEPT;
-
 /**
  * Common utilities
  */
 public class Utils {
-    public static boolean isGETFromCurrentHostAndAcceptsHTML(URI baseURI, HttpRequest httpRequest) {
-        boolean methodIsGet = httpRequest.getMethod().equals(HttpMethod.GET);
-        boolean expectsHtmlContent = httpRequest.headers().get(ACCEPT).contains(Constants.HTML);
-        boolean isFromCurrentBaseUrl = httpRequest.getUri().contains(baseURI.getHost());
-        return expectsHtmlContent && isFromCurrentBaseUrl && methodIsGet;
-    }
-
     public static double toSeconds(long nanoseconds) {
         return nanoseconds / 1000000000.0;
     }
--- bromium-core/src/test/java/com/hribol/bromium/core/utils/GetHtmlFromCurrentHostPredicateTest.java
+++ bromium-core/src/test/java/com/hribol/bromium/core/utils/GetHtmlFromCurrentHostPredicateTest.java
@@ -9,6 +9,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -54,6 +55,34 @@ public class GetHtmlFromCurrentHostPredicateTest {
         when(httpRequest.getUri()).thenReturn("http://tenniskafe.com/atp");
         when(httpRequest.headers()).thenReturn(httpHeaders);
         when(httpRequest.getMethod()).thenReturn(HttpMethod.GET);
+
+        GetHtmlFromCurrentHostPredicate getHtmlFromCurrentHostPredicate = new GetHtmlFromCurrentHostPredicate(uri);
+        assertFalse(getHtmlFromCurrentHostPredicate.test(httpRequest));
+    }
+
+    @Test
+    public void whenHostIsDifferentAndContentTypeIsNotHTPNotChangingURLRequest() throws URISyntaxException {
+        URI uri = new URI("http://tenniskafe.com");
+        HttpHeaders httpHeaders = mock(HttpHeaders.class);
+        when(httpHeaders.get("Accept")).thenReturn("application/html");
+        HttpRequest httpRequest = mock(HttpRequest.class);
+        when(httpRequest.getUri()).thenReturn("http://tenniskafe.com");
+        when(httpRequest.headers()).thenReturn(httpHeaders);
+        when(httpRequest.getMethod()).thenReturn(HttpMethod.GET);
+
+        GetHtmlFromCurrentHostPredicate getHtmlFromCurrentHostPredicate = new GetHtmlFromCurrentHostPredicate(uri);
+        assertTrue(getHtmlFromCurrentHostPredicate.test(httpRequest));
+    }
+
+    @Test
+    public void ifRequestIsNotGetThenItIsFalse() throws URISyntaxException {
+        URI uri = new URI("http://tenniskafe.com");
+        HttpHeaders httpHeaders = mock(HttpHeaders.class);
+        when(httpHeaders.get("Accept")).thenReturn("application/html");
+        HttpRequest httpRequest = mock(HttpRequest.class);
+        when(httpRequest.getUri()).thenReturn("http://tenniskafe.com");
+        when(httpRequest.headers()).thenReturn(httpHeaders);
+        when(httpRequest.getMethod()).thenReturn(HttpMethod.POST);
 
         GetHtmlFromCurrentHostPredicate getHtmlFromCurrentHostPredicate = new GetHtmlFromCurrentHostPredicate(uri);
         assertFalse(getHtmlFromCurrentHostPredicate.test(httpRequest));
