37de27506fd778d80aaca7aeff23875da9bde240
Hristo Vrigazov <hvrigazov@gmail.com>
2017-08-06 15:27:28 +0300

    GH-16: Added unit test for RecordRequestFilter when the encoding is not supported

--- bromium-record/src/test/java/com/hribol/bromium/record/RecordRequestFilterTest.java
+++ bromium-record/src/test/java/com/hribol/bromium/record/RecordRequestFilterTest.java
@@ -75,6 +75,44 @@ public class RecordRequestFilterTest {
         verify(recordingState, never()).storeTestCaseStep(anyMap());
     }
 
+    @Test
+    public void doesNotAddToStateIfConverterReturnsEmpty() throws MalformedURLException, UnsupportedEncodingException {
+        HttpRequest httpRequest = mock(HttpRequest.class);
+        Map<String, String> event = getEvent();
+        when(httpRequest.getUri()).thenReturn(SUBMIT_EVENT_URL + "?event=mockEvent&text=mockText");
+        Predicate<HttpRequest> httpRequestPredicate = mock(Predicate.class);
+        when(httpRequestPredicate.test(httpRequest)).thenReturn(true);
+        RecordingState recordingState = mock(RecordingState.class);
+        HttpRequestToTestCaseStepConverter httpRequestToTestCaseStepConverter = mock(HttpRequestToTestCaseStepConverter.class);
+        when(httpRequestToTestCaseStepConverter.convert(httpRequest)).thenReturn(Optional.empty());
+        EventDetector eventDetector = mock(EventDetector.class);
+        when(eventDetector.canDetectPredicate()).thenReturn(httpRequestPredicate);
+        when(eventDetector.getConverter()).thenReturn(httpRequestToTestCaseStepConverter);
+        List<EventDetector> eventDetectors = Arrays.asList(eventDetector);
+        RecordRequestFilter requestFilter = new RecordRequestFilter(recordingState, eventDetectors);
+        requestFilter.filterRequest(httpRequest, mock(HttpMessageContents.class), mock(HttpMessageInfo.class));
+
+        verify(recordingState, never()).storeTestCaseStep(event);
+    }
+
+    @Test
+    public void doesNotAddToQueueIfEncodingIsUnsupporte() throws MalformedURLException, UnsupportedEncodingException {
+        HttpRequest httpRequest = mock(HttpRequest.class);
+        when(httpRequest.getUri()).thenReturn("blabla" + SUBMIT_EVENT_URL);
+        RecordingState recordingState = mock(RecordingState.class);
+        Predicate<HttpRequest> httpRequestPredicate = mock(Predicate.class);
+        when(httpRequestPredicate.test(httpRequest)).thenReturn(true);
+        HttpRequestToTestCaseStepConverter httpRequestToTestCaseStepConverter = mock(HttpRequestToTestCaseStepConverter.class);
+        when(httpRequestToTestCaseStepConverter.convert(httpRequest)).thenThrow(new UnsupportedEncodingException());
+        EventDetector eventDetector = mock(EventDetector.class);
+        when(eventDetector.canDetectPredicate()).thenReturn(httpRequestPredicate);
+        when(eventDetector.getConverter()).thenReturn(httpRequestToTestCaseStepConverter);
+        List<EventDetector> eventDetectors = Arrays.asList(eventDetector);
+        RecordRequestFilter requestFilter = new RecordRequestFilter(recordingState, eventDetectors);
+        requestFilter.filterRequest(httpRequest, mock(HttpMessageContents.class), mock(HttpMessageInfo.class));
+        verify(recordingState, never()).storeTestCaseStep(anyMap());
+    }
+
     private Map<String, String> getEvent() {
         Map<String, String> event = new HashMap<>();
         event.put("event", "mockEvent");
