aab5eabc15b66e0fe39e9883c9db585bcf8fb33f
Hristo Vrigazov <hvrigazov@sdl.com>
2017-10-02 16:16:01 +0300

    GH-52: Document scenario factory and action factory

--- bromium-replay/src/main/java/com/hribol/bromium/replay/execution/factory/WebDriverActionFactory.java
+++ bromium-replay/src/main/java/com/hribol/bromium/replay/execution/factory/WebDriverActionFactory.java
@@ -8,5 +8,13 @@ import java.util.Map;
  * A factory for creating {@link WebDriverAction}
  */
 public interface WebDriverActionFactory {
+
+    /**
+     * Creates a web driver action
+     * @param webdriverActionType the type of the action, for example "CLICK_CLASS_BY_TEXT"
+     * @param parameters the parameters for the action in raw format
+     * @param expectsHttpRequest whether the action should expect http request after it is executed
+     * @return the web driver action
+     */
     WebDriverAction create(String webdriverActionType, Map<String, String> parameters, boolean expectsHttpRequest);
 }
