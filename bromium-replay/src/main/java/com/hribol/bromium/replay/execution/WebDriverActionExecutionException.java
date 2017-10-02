cf7e1e2dbc29fc20a99db8db627b9a500ff769c5
Hristo Vrigazov <hvrigazov@sdl.com>
2017-10-02 17:09:24 +0300

    GH-52: Document WebDriverActionExecutionException

--- bromium-replay/src/main/java/com/hribol/bromium/replay/execution/WebDriverActionExecutionException.java
+++ bromium-replay/src/main/java/com/hribol/bromium/replay/execution/WebDriverActionExecutionException.java
@@ -3,17 +3,27 @@ package com.hribol.bromium.replay.execution;
 import com.hribol.bromium.replay.report.AutomationResult;
 
 /**
- * Created by hvrigazov on 20.05.17.
+ * A runtime exception while executing a scenario
  */
 public class WebDriverActionExecutionException extends RuntimeException {
 
     private AutomationResult automationResult;
 
+    /**
+     * Creates a new exception
+     * @param message description of the exception
+     * @param cause cause of the error
+     * @param automationResultBuilder an instance which converts the cause to a meaningful {@link AutomationResult}
+     */
     public WebDriverActionExecutionException(String message, Throwable cause, AutomationResultBuilder automationResultBuilder) {
         super(message, cause);
         this.automationResult = automationResultBuilder.buildAutomationResult(cause);
     }
 
+    /**
+     * Gets the automation result
+     * @return meaningful automation result
+     */
     public AutomationResult getAutomationResult() {
         return automationResult;
     }
