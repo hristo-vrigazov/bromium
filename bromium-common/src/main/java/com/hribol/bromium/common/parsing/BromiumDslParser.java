265d9b5e013b6c0b885ab76af1f50ababb1531b1
Hristo Vrigazov <hvrigazov@gmail.com>
2017-10-27 15:07:45 +0300

    GH-32: Added base test for DSL parsing

---
+++ bromium-common/src/main/java/com/hribol/bromium/common/parsing/BromiumDslParser.java
@@ -0,0 +1,56 @@
+package com.hribol.bromium.common.parsing;
+
+import com.google.inject.Injector;
+import com.hribol.bromium.core.config.ApplicationConfiguration;
+import com.hribol.bromium.core.parsing.ApplicationConfigurationParser;
+import com.hribol.bromium.dsl.BromiumStandaloneSetup;
+import com.hribol.bromium.dsl.bromium.Model;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.xtext.util.CancelIndicator;
+import org.eclipse.xtext.validation.CheckMode;
+import org.eclipse.xtext.validation.IResourceValidator;
+import org.eclipse.xtext.validation.Issue;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by hvrigazov on 26.10.17.
+ */
+public class BromiumDslParser implements ApplicationConfigurationParser {
+
+    private ResourceSet resourceSet;
+    private IResourceValidator validator;
+
+    public BromiumDslParser() {
+        Injector injector = new BromiumStandaloneSetup().createInjectorAndDoEMFRegistration();
+        resourceSet = injector.getInstance(ResourceSet.class);
+        validator = injector.getInstance(IResourceValidator.class);
+    }
+
+    @Override
+    public ApplicationConfiguration parseApplicationConfiguration(File file) throws IOException {
+        Resource resource = resourceSet.getResource(URI.createFileURI(file.getAbsolutePath()), true);
+
+        List<Issue> issues = validator.validate(resource, CheckMode.ALL, CancelIndicator.NullImpl);
+
+        if (!issues.isEmpty()) {
+            throw new IllegalStateException("Invalid bromium configuration. " + issues);
+        }
+
+        Model model = (Model) resource.getAllContents().next();
+
+        ApplicationConfiguration applicationConfiguration = new ApplicationConfiguration();
+        applicationConfiguration.setApplicationName(model.getName());
+        applicationConfiguration.setVersion(model.getVersion().toString());
+        return applicationConfiguration;
+    }
+
+    @Override
+    public ApplicationConfiguration parseApplicationConfiguration(String filename) throws IOException {
+        return null;
+    }
+}
---
+++ bromium-common/src/test/java/com/hribol/bromium/common/parsing/BromiumDslParserTest.java
@@ -0,0 +1,32 @@
+package com.hribol.bromium.common.parsing;
+
+import com.hribol.bromium.core.config.ApplicationConfiguration;
+import com.hribol.bromium.core.parsing.ApplicationConfigurationParser;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by hvrigazov on 26.10.17.
+ */
+public class BromiumDslParserTest {
+
+    public static final String EXAMPLE_NAME = "Example name";
+    public static final String EXAMPLE_VERSION = "8.2.5";
+
+    @Test
+    public void parsesNameAndThreeDottedVersionCorrectly() throws IOException {
+        File file = new File(getClass().getResource("/name.brm").getFile());
+
+        ApplicationConfigurationParser parser = new BromiumDslParser();
+
+        ApplicationConfiguration applicationConfiguration = parser.parseApplicationConfiguration(file);
+
+        assertEquals(EXAMPLE_NAME, applicationConfiguration.getApplicationName());
+        assertEquals(EXAMPLE_VERSION, applicationConfiguration.getVersion());
+    }
+
+}
