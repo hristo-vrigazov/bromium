1a7ab5739fa0e0315450162d0357007c3dad78f0
Hristo Vrigazov <hvrigazov@gmail.com>
2017-10-28 21:09:21 +0300

    GH-32: Added unit test for DslStepsDumper

--- bromium-common/src/test/java/com/hribol/bromium/common/parsing/DslStepsDumperTest.java
+++ bromium-common/src/test/java/com/hribol/bromium/common/parsing/DslStepsDumperTest.java
@@ -25,12 +25,15 @@ public class DslStepsDumperTest {
 
     private static final String loadPageActionName = "loadPage";
     private static final String typeInNameInputActionName = "typeInNameInput";
+    private static final String clickLoginButtonActionName = "clickLoginButtonActionName";
+    private static final String outputFilename = "tmp.json";
 
     private static final String aliasUrl = "alias-url";
     private static final String aliasUrlValue = "text-field.html";
 
     private static final String aliasText = "alias-text";
     private static final String aliasTextValue = "admin";
+
 
     @Test
     public void dumpsStepsReplacingAliases() throws IOException {
@@ -46,38 +49,57 @@ public class DslStepsDumperTest {
         testScenarioSteps.add(firstStep);
         testScenarioSteps.add(secondStep);
 
-        String outputFilename = "tmp.json";
         File outputFile = new File(outputFilename);
 
-        Map<String, ApplicationActionConfiguration> actionConfigurationMap = createMockConfiguration();
+        Map<String, ApplicationActionConfiguration> actionConfigurationMap = createAliasesMockConfiguration();
 
         StepsDumper stepsDumper = new DslStepsDumper(actionConfigurationMap);
-
         stepsDumper.dump(testScenarioSteps, outputFilename);
 
         List<String> lines = Files.readLines(outputFile, Charsets.UTF_8);
+        assertEquals(2, lines.size());
         assertEquals("Load text-field.html page ", lines.get(0));
         assertEquals("Type admin in name input ", lines.get(1));
 
         assertTrue(outputFile.delete());
     }
 
-    private Map<String, ApplicationActionConfiguration> createMockConfiguration() {
-        SyntaxDefinitionConfiguration loadSyntax = mock(SyntaxDefinitionConfiguration.class);
-        when(loadSyntax.getContent()).thenReturn("Load");
-        when(loadSyntax.getExposedParameter()).thenReturn(aliasUrl);
+    @Test
+    public void correctlyHandlesNoAliases() throws IOException {
+        Map<String, String> firstStep = new HashMap<>();
+        firstStep.put(EVENT, clickLoginButtonActionName);
 
-        SyntaxDefinitionConfiguration finishLoadSyntax = mock(SyntaxDefinitionConfiguration.class);
-        when(finishLoadSyntax.getContent()).thenReturn("page");
-        when(finishLoadSyntax.getExposedParameter()).thenReturn(null);
+        TestScenarioSteps testScenarioSteps = new TestScenarioSteps();
+        testScenarioSteps.add(firstStep);
 
-        List<SyntaxDefinitionConfiguration> loadPageSyntaxDefinitions = new ArrayList<>();
-        loadPageSyntaxDefinitions.add(loadSyntax);
-        loadPageSyntaxDefinitions.add(finishLoadSyntax);
+        File outputFile = new File(outputFilename);
 
-        ApplicationActionConfiguration loadPageAction = mock(ApplicationActionConfiguration.class);
-        when(loadPageAction.getSyntaxDefinitionConfigurationList()).thenReturn(loadPageSyntaxDefinitions);
+        Map<String, ApplicationActionConfiguration> actionConfigurationMap = createNoAliasesMockConfiguration();
 
+        StepsDumper stepsDumper = new DslStepsDumper(actionConfigurationMap);
+        stepsDumper.dump(testScenarioSteps, outputFilename);
+
+        List<String> lines = Files.readLines(outputFile, Charsets.UTF_8);
+        assertEquals(1, lines.size());
+        assertEquals("Click login button ", lines.get(0));
+        assertTrue(outputFile.delete());
+
+    }
+
+    private Map<String, ApplicationActionConfiguration> createNoAliasesMockConfiguration() {
+        Map<String, ApplicationActionConfiguration> actionConfigurationMap = new HashMap<>();
+        actionConfigurationMap.put(clickLoginButtonActionName, getClickLoginButtonAction());
+        return actionConfigurationMap;
+    }
+
+    private Map<String, ApplicationActionConfiguration> createAliasesMockConfiguration() {
+        Map<String, ApplicationActionConfiguration> actionConfigurationMap = new HashMap<>();
+        actionConfigurationMap.put(loadPageActionName, getLoadPageAction());
+        actionConfigurationMap.put(typeInNameInputActionName, getTypeIntoTextFieldAction());
+        return actionConfigurationMap;
+    }
+
+    private ApplicationActionConfiguration getTypeIntoTextFieldAction() {
         SyntaxDefinitionConfiguration typeSyntax = mock(SyntaxDefinitionConfiguration.class);
         when(typeSyntax.getContent()).thenReturn("Type");
         when(typeSyntax.getExposedParameter()).thenReturn(aliasText);
@@ -92,11 +114,37 @@ public class DslStepsDumperTest {
 
         ApplicationActionConfiguration typeIntoTextFieldAction = mock(ApplicationActionConfiguration.class);
         when(typeIntoTextFieldAction.getSyntaxDefinitionConfigurationList()).thenReturn(typeIntoTextFieldSyntaxDefinition);
-
-        Map<String, ApplicationActionConfiguration> actionConfigurationMap = new HashMap<>();
-        actionConfigurationMap.put(loadPageActionName, loadPageAction);
-        actionConfigurationMap.put(typeInNameInputActionName, typeIntoTextFieldAction);
-        return actionConfigurationMap;
+        return typeIntoTextFieldAction;
     }
 
+    private ApplicationActionConfiguration getLoadPageAction() {
+        SyntaxDefinitionConfiguration loadSyntax = mock(SyntaxDefinitionConfiguration.class);
+        when(loadSyntax.getContent()).thenReturn("Load");
+        when(loadSyntax.getExposedParameter()).thenReturn(aliasUrl);
+
+        SyntaxDefinitionConfiguration finishLoadSyntax = mock(SyntaxDefinitionConfiguration.class);
+        when(finishLoadSyntax.getContent()).thenReturn("page");
+        when(finishLoadSyntax.getExposedParameter()).thenReturn(null);
+
+        List<SyntaxDefinitionConfiguration> loadPageSyntaxDefinitions = new ArrayList<>();
+        loadPageSyntaxDefinitions.add(loadSyntax);
+        loadPageSyntaxDefinitions.add(finishLoadSyntax);
+
+        ApplicationActionConfiguration loadPageAction = mock(ApplicationActionConfiguration.class);
+        when(loadPageAction.getSyntaxDefinitionConfigurationList()).thenReturn(loadPageSyntaxDefinitions);
+        return loadPageAction;
+    }
+
+    public ApplicationActionConfiguration getClickLoginButtonAction() {
+        SyntaxDefinitionConfiguration syntaxDefinitionConfiguration = mock(SyntaxDefinitionConfiguration.class);
+        when(syntaxDefinitionConfiguration.getContent()).thenReturn("Click login button");
+        when(syntaxDefinitionConfiguration.getExposedParameter()).thenReturn(null);
+
+        List<SyntaxDefinitionConfiguration> syntaxDefinitionConfigurations = new ArrayList<>();
+        syntaxDefinitionConfigurations.add(syntaxDefinitionConfiguration);
+
+        ApplicationActionConfiguration action = mock(ApplicationActionConfiguration.class);
+        when(action.getSyntaxDefinitionConfigurationList()).thenReturn(syntaxDefinitionConfigurations);
+        return action;
+    }
 }
