855227fef4ef4708d6d6849153c4d19b2e95899f
Hristo Vrigazov <hvrigazov@gmail.com>
2017-11-24 22:45:11 +0200

    GH-9: Immediately return

--- bromium-replay/src/main/java/com/hribol/bromium/replay/filters/ReplayRequestFilter.java
+++ bromium-replay/src/main/java/com/hribol/bromium/replay/filters/ReplayRequestFilter.java
@@ -2,8 +2,10 @@ package com.hribol.bromium.replay.filters;
 
 import com.google.inject.Inject;
 import com.hribol.bromium.replay.ReplayingState;
+import io.netty.handler.codec.http.DefaultHttpResponse;
 import io.netty.handler.codec.http.HttpRequest;
 import io.netty.handler.codec.http.HttpResponse;
+import io.netty.handler.codec.http.HttpResponseStatus;
 import net.lightbody.bmp.filters.RequestFilter;
 import net.lightbody.bmp.util.HttpMessageContents;
 import net.lightbody.bmp.util.HttpMessageInfo;
@@ -41,6 +43,7 @@ public class ReplayRequestFilter implements RequestFilter {
                     URL url = new URL(httpRequest.getUri());
                     String event = url.getQuery();
                     conditionsUpdater.updater().update(replayingState, event);
+                    return new DefaultHttpResponse(httpRequest.getProtocolVersion(), HttpResponseStatus.OK);
                 } catch (MalformedURLException e) {
                     logger.error(e.getMessage(), e);
                 }
