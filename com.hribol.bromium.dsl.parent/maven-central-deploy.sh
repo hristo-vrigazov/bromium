a02bf61e9538a7f736a6007f180159e6cf0f1321
hristo-vrigazov <hvrigazov@gmail.com>
2017-12-18 22:20:33 +0200

    Updated scripts for deployment

---
+++ com.hribol.bromium.dsl.parent/maven-central-deploy.sh
@@ -0,0 +1,13 @@
+# Deploy maven artefact in current directory into Maven central repository 
+# using maven-release-plugin goals
+
+read -p "Really deploy to maven cetral repository  (yes/no)? "
+
+if ( [ "$REPLY" == "yes" ] ) then
+	ssh-add ~/.ssh/github_rsa
+	ssh-add -l
+	mvn -Prelease-sign-artifacts release:prepare release:perform -B -e | tee maven-central-deploy.log
+	ssh-add -D
+else
+	echo 'Exit without deploy'
+fi
